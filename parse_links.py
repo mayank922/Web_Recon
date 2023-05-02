85680f7234492a3c5e49c673408859d57fdf1258
mayank922 <rajmayank432@gmail.com>
2023-05-01 22:26:25 -0400

    search

--- parse_links.py
+++ parse_links.py
@@ -4,20 +4,11 @@ import os
 import argparse
 import re
 
-
+# can try to print emails seperaterly
 def p_links(url):
     anon = parse.browser_handle(url)
     try:
-        print('[+] Printing Links From Regex.')
-        link_finder = r'href="(.*?)"'
-        links = re.findall(link_finder, anon)
-        print(*links, sep='\n')
-    except Exception as e:
-        print(f'{"":>3}[-] Exception: {e.__class__.__name__}')
-        pass
-
-    try:
-        print('\n[+] Printing Links From BeautifulSoup.')
+        print('\n[+] Printing Links and emails From BeautifulSoup.')
         soup = BeautifulSoup(anon, 'html.parser')
         links = [link.get('href') for link in soup.find_all('a', href=True)]
         print(*links, sep='\n')
@@ -28,6 +19,6 @@ def p_links(url):
 
 
 
-url = 'https://seedsecuritylabs.org/'
+url = 'https://www.3i-infotech.com'
 p_links(url)
 
