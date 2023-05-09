f96565a8235605c5ccbdf1b01fb4251075d016b4
mayank922 <rajmayank432@gmail.com>
2023-05-09 14:33:46 -0400

    May 9

--- Webcon.py
+++ Webcon.py
@@ -1,20 +1,21 @@
 import argparse
 import parse_links
 import extract_email
-import openports
+#import openports
 import googleapi
 import images_downloader
+import server_info
 from art import *
 
 if __name__ == "__main__":
     tprint("Web-Con",font="3d_diognal")
-    parser = argparse.ArgumentParser(description="Will be edited")
+    parser = argparse.ArgumentParser(description="The web-con tool extracts information like IP address, open ports, email addresses, and more from a target web app. We use advanced techniques, external APIs, and anonymity measures to gather data non-intrusively.")
     parser.add_argument("-l" ,required=False, dest="links", help="specify the domain")
     parser.add_argument("-e" ,required=False, dest="emails" ,help="specify the domain" )
     parser.add_argument("-p" ,required=False, dest="port" ,help="specify the domain")
     parser.add_argument("-g" ,required=False, dest="gsearch" ,help="specify the query of choice")
     parser.add_argument("-i" ,required=False, dest="img" ,help="specify the domain")
-    parser.add_argument("-a" ,required=False, dest="all_out" ,help="specify the domain")
+    parser.add_argument("-s" ,required=False, dest="server" ,help="specify the domain")
     
 
     args = parser.parse_args()
@@ -23,7 +24,8 @@ if __name__ == "__main__":
     port = args.port
     gsearch = args.gsearch
     img = args.img
-    all_out = args.all_out
+    #all_out = args.all_out
+    server = args.server
     if links:
         parse_links.p_links(links)
     
@@ -40,14 +42,17 @@ if __name__ == "__main__":
     if img:
         images_downloader.main(img)
 
-    if all_out:
-        openports.scan_port(port)
-        print("------------------------")
-        parse_links.p_links(links)
-        print("------------------------")
-        extract_email.extract_email(emails)
-        print("------------------------")
-        googleapi.query_search(gsearch)
-        print("------------------------")
-        images_downloader.main(img)
-        print("------------------------")
+    if server:
+        server_info.serverinfo(server)
+
+    # if all_out:
+    #     openports.scan_port(port)
+    #     print("------------------------")
+    #     parse_links.p_links(links)
+    #     print("------------------------")
+    #     extract_email.extract_email(emails)
+    #     print("------------------------")
+    #     googleapi.query_search(gsearch)
+    #     print("------------------------")
+    #     images_downloader.main(img)
+    #     print("------------------------")
--- server_info.py
+++ server_info.py
@@ -1,12 +1,11 @@
 import builtwith
 import requests
 
-target_website = "192.168.116.134"
-
 def serverinfo(target_website):
     result = builtwith.builtwith("https://www." + target_website)
     #options=["font-scripts","tag-managers" ,"photo-galleries" , "javascript-frameworks" ,"widgets","web-frameworks","cms","programming-languages","blogs","marketing-automation"]
     x= " , ".join(result["web-frameworks"])
+    
     y =" ".join(result["programming-languages"])
     print("Programming language used by the webpage -->" + y)
     print("Web frameworks used by the webpage --> " + x)
@@ -15,4 +14,20 @@ def serverinfo(target_website):
     print("Server used by the webpage --> " + response.headers["server"])
 
 
-serverinfo(target_website)
+# import urllib.request, json 
+# target_website = "192.168.116.134"
+#bw = BuiltWith("c65ba0f5-298b-4c68-8abf-76cc33a57b2c")
+
+# response = requests.get("https://api.builtwith.com/free1/api.json?KEY=c65ba0f5-298b-4c68-8abf-76cc33a57b2c&LOOKUP=hackthissite.org")
+# #print(respones.json())
+# if response.status_code == 200:
+#     # Print the technology tags for the website
+#     for tag in response.json()["Results"][0]["Result"]["Tags"]:
+#         print(tag["Name"])
+# else:
+#     print("Failed to retrieve data from BuiltWith API")
+
+
+# with urllib.request.urlopen("https://api.builtwith.com/free1/api.json?KEY=c65ba0f5-298b-4c68-8abf-76cc33a57b2c&LOOKUP=vulnweb.com") as url:
+#     data = json.load(url)
+#     print(data)
